caefa74a0a58563669eccc60be91750f73edd1e3
Conor <ckear1989@gmail.com>
2021-11-03 09:16:42 +0000

    add pre-commit

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setuptools.setup(
     name="github",
     extras_require={
         "dev": [
+            "pre-commit",
+            "pre-commit-hooks",
             "PyGitHub",
         ],
         "test": [],
