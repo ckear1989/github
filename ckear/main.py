43b5cba0a1b5f34442e3140eb3d2fa4eb6df7cc6
Conor <ckear1989@gmail.com>
2021-11-07 14:20:56 +0000

    initial parsing of repo details in pandas DataFrame

--- ckear/main.py
+++ ckear/main.py
@@ -1,8 +1,10 @@
 """
-Empty module docstring.
+Helper functions to parse repo details.
+Main function handles logic to connect to GitHub and select repos for analysis.
 """
 import os
 
+import pandas as pd
 from github import Github
 
 
@@ -16,20 +18,46 @@ def get_connection():
     return github_con
 
 
+def get_branch_details(branch):
+    """
+    Get information on branch from PyGitHub API and format in pandas DataFrame.
+    """
+    branch_dict = {
+        "branch": [branch.name],
+    }
+    branch_df = pd.DataFrame.from_dict(branch_dict)
+    return branch_df
+
+
+def get_repo_details(repo):
+    """
+    Get information on repo from PyGitHub API and format in pandas DataFrame.
+    """
+    branch_df = pd.concat(
+        [get_branch_details(branch) for branch in repo.get_branches()],
+        ignore_index=True,
+    )
+    repo_dict = {
+        "repo": [repo.name],
+        "private": [repo.private],
+        "branch_count": [len(branch_df)],
+    }
+    repo_df = pd.DataFrame.from_dict(repo_dict)
+    return repo_df
+
+
 def main():
     """
-    Empty docstring.
+    Main method to parse repo details into pandas DataFrame.
     """
     github_con = get_connection()
     user = github_con.get_user()
-    print(dir(user))
-    # Then play with your Github objects:
-    for repo in github_con.get_user().get_repos():
-        print(repo.name)
-        # print(dir(repo))
-    for org in github_con.get_user().get_orgs():
-        print(org.name)
-        print(dir(org))
+    repo_df = pd.concat(
+        [get_repo_details(repo) for repo in user.get_repos()], ignore_index=True
+    )
+    print(repo_df)
+    for org in user.get_orgs():
+        print(org)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Empty module docstring.
+Setup for pip package installation.
 """
 
 import setuptools
@@ -12,6 +12,7 @@ setuptools.setup(
             "pre-commit-hooks>=4.0.1",
             "PyGitHub>=1.55",
             "pylint>=2.11.1",
+            "pandas>=1.3.4",
         ],
         "test": [
             "pytest>=6.2.5",
