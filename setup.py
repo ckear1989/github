fb9eac02fd97c444368c124cde441ef17ec13963
Conor <ckear1989@gmail.com>
2021-11-10 12:47:32 +0000

    add run dependencies properly

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ setuptools.setup(
     author_email="ckear1989@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=[
+        "PyGitHub==1.55",
+        "pandas==1.3.4",
+        "flask==2.0.2",
+        "altair==4.1.0",
+    ],
     extras_require={
-        "run": [
-            "PyGitHub==1.55",
-            "pandas==1.3.4",
-            "flask==2.0.2",
-            "altair==4.1.0",
-        ],
         "dev": [
             "pre-commit>=2.15.0",
             "PyGitHub>=1.55",
