aa227a38201af8ebe0ac6669565b316c891c5736
Conor <ckear1989@gmail.com>
2021-11-20 14:35:08 +0000

    deployment helpers

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,38 @@
 #!/usr/bin/bash
 
-tag=$1
+# dev
+tag="0.0.0."`date '+%s'`
 git tag $tag
-git push --tags
 conda env remove -n deploy
 conda create -n deploy python==3.8 -y
 conda activate deploy
 pip install build
 pip install setuptools_scm
 python -m build
-pip install .[deploy]
-pip freeze > GitHubHealth/app/requirements2.txt
-diff GitHubHealth/app/requirements.txt GitHubHealth/app/requirements2.txt
-mv GitHubHealth/app/requirements2.txt GitHubHealth/app/requirements.txt
-twine check dist/GitHubHealth*
-twine upload dist/GitHubHealth*
-cd GitHubHealth/app && gcloud app deploy && cd -
-# cd GitHubHealth/app && gcloud run deploy && cd -
+git tag -d $tag
+twine check dist/*
+twine upload dist/*
+pip install -e .[deploy]
+cd GitHubHealth/app
+pip freeze > requirements2.txt
+diff requirements.txt requirements2.txt
+mv requirements2.txt requirements.txt
 conda deactivate
+cd -
+
+# check everything has gone ok up until now
+# the version in requirements.txt should match the tag just created
+# then do the following:
+
+# test locally:
+# python -m GitHubHealth.app.main
+
+# deploy "app" for dev or "run" for prd
+# follow prompts and check url
+# gcloud [app|run] deploy
+
+# TODOs:
+# automated timestamp tag for dev and user specified version for prod
+# figure out DNS settings for different versions of the deployed app
+# including aliases, CNAMES etc.
+# how to use the app.yaml file properly
