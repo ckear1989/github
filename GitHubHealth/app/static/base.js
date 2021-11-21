fc1ad1ac1d584d9e9b34caefd7afbe2ef679b01d
Conor <ckear1989@gmail.com>
2021-11-21 09:34:55 +0000

    fix typo in sourcing javascriptwq
    add toggleMode and settings to base.js

--- GitHubHealth/app/static/base.js
+++ GitHubHealth/app/static/base.js
@@ -2,3 +2,13 @@ function loading(){
     $("#loading").show();
     $("#content").hide();
 }
+
+function settings() {
+    document.getElementById("settingsDropdown").classList.toggle("show");
+}
+
+function toggleMode() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    document.getElementById("settingsDropdown").classList.toggle("hide");
+}
