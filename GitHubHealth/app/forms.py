cd052cd485cbaa07fa53b8bcce39b2c4341e7346
Conor <ckear1989@gmail.com>
2021-11-21 16:43:45 +0000

    inherit form validation correctly.  format search form (manually) :(

--- GitHubHealth/app/forms.py
+++ GitHubHealth/app/forms.py
@@ -19,14 +19,10 @@ class LoginForm(FlaskForm):
 
     login_user = StringField("user login", [validators.DataRequired()])
     gat = PasswordField("github token", [validators.DataRequired()])
-    # remember_me = BooleanField('Remember Me')
     login = SubmitField(render_kw={"onclick": "loading()"})
 
-    # will fix this at some stage
-    # pylint: disable=bad-super-call
-    # pylint: disable=arguments-differ
-    def validate(self):
-        if not super(FlaskForm, self).validate():
+    def validate(self, extra_validators=None):
+        if not super().validate(extra_validators=extra_validators):
             return False
         if not self.login_user.data or not self.gat.data:
             msg = "Log in using GitHub username and access token"
@@ -41,18 +37,15 @@ class SearchForm(FlaskForm):
     Form for github user class.
     """
 
-    search_request = StringField("search")
+    search_request = StringField()
     search_users = BooleanField("users")
     search_orgs = BooleanField("orgs")
     search_teams = BooleanField("teams")
-    search_ignore_repos = StringField("ignore repos")
+    search_ignore_repos = StringField("ignore repos", id="ignore-repos")
     search = SubmitField(render_kw={"onclick": "loading()"})
 
-    # will fix this at some stage
-    # pylint: disable=bad-super-call
-    # pylint: disable=arguments-differ
-    def validate(self):
-        if not super(FlaskForm, self).validate():
+    def validate(self, extra_validators=None):
+        if not super().validate(extra_validators=extra_validators):
             return False
         if not self.search.data:
             msg = "Please enter search term."
