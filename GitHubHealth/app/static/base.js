00944deff2170ee7d4d32a32e4ff8de4c6498fd8
Conor <ckear1989@gmail.com>
2021-11-27 14:33:12 +0000

    theme toggle buttons

--- GitHubHealth/app/static/base.js
+++ GitHubHealth/app/static/base.js
@@ -3,14 +3,58 @@ function loading(){
     $("#content").hide();
 }
 
-  function settings() {
+function settings() {
     document.getElementById("settingsDropdown").classList.toggle("show");
-  }
-  function toggleMode() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-    document.getElementById("settingsDropdown").classList.toggle("hide");
+}
 
-    var isLight = element.classList.contains("light-mode");
-    localStorage.setItem("lightMode", isLight);
-  }
+function activateLightMode() {
+    var element = document.body;
+    // toggle light-mode
+    element.classList.toggle("light-mode");
+    // if it is now active set class for colors
+    let currently_active = element.classList.contains("light-mode")
+    if (currently_active) {
+        let inel = document.getElementById("lightMode");
+        inel.className = "active";
+        inel.setAttribute("class", "theme-pick-chosen")
+    } else {
+        let inel = document.getElementById("lightMode");
+        inel.className = "inactive";
+        inel.setAttribute("class", "theme-pick")
+    }
+
+    // deactivate dark-mode
+    element.classList.remove("dark-mode");
+    let outel = document.getElementById('darkMode');
+    outel.className = 'inactive';
+    outel.setAttribute("class", "theme-pick")
+
+    localStorage.setItem("lightMode", true);
+    localStorage.setItem("darkMode", false);
+}
+
+function activateDarkMode() {
+    var element = document.body;
+    // toggle dark-mode
+    element.classList.toggle("dark-mode");
+    // if it is now active set class for colors
+    let currently_active = element.classList.contains("dark-mode")
+    if (currently_active) {
+        let inel = document.getElementById("darkMode");
+        inel.className = "active";
+        inel.setAttribute("class", "theme-pick-chosen")
+    } else {
+        let inel = document.getElementById("darkMode");
+        inel.className = "inactive";
+        inel.setAttribute("class", "theme-pick")
+    }
+
+    // deactivate light-mode
+    element.classList.remove("light-mode");
+    let outel = document.getElementById('lightMode');
+    outel.className = "inactive";
+    outel.setAttribute("class", "theme-pick");
+
+    localStorage.setItem("darkMode", true);
+    localStorage.setItem("lightMode", false);
+}
